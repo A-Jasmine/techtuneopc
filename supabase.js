0c66ea08f2d1b446fa8a243d26717afb690cdcde
A-Jasmine <altheajjjz@gmail.com>
2026-05-09 15:21:09 +0800

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // =============== SUPABASE CONFIG ===============
 // Fill these from Supabase > Project Settings > API
-const SUPABASE_URL = "https://vnxzrbcctnsnacbobepk.supabase.com";
-const SUPABASE_ANON_KEY = "sb_publishable_6dZkRXqDe-ubVWIdoYcAfw_EQoQdFvA";
+const SUPABASE_URL = "https://ywupquhxwosxmcimnajp.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_vRDmqLYEOrXvGRBOeYlJQQ_B3ckuMzS";
 
 const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 window.sb = sb;
